313f0d4ef7b6076e02b51ac6f132f33f71eab922
Grace Joh <graceeunaejoh@gmail.com>
2022-05-26 01:09:57 -0400

    modified gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,15 @@
 class PostsController < ApplicationController
+  require "rest-client"
+
+  def get_posts
+    url = "https://api.hatchways.io/assessment/blog/posts"
+    response = RestClient.get(url)
+    render json: response
+  end
+
+  # def get_posts
+  #   url = "https://api.hatchways.io/assessment/blog/posts"
+  #   query_params = { 'tag': "tech" }
+  #   response = RestClient.get(url, query: query_params)
+  # end
 end
