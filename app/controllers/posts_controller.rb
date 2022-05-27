b8e111a410f6836e948bf413bf902b6643dbd3f4
Grace Joh <graceeunaejoh@gmail.com>
2022-05-26 22:52:40 -0400

    search sort by directions params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,14 +2,24 @@ class PostsController < ApplicationController
   require "rest-client"
 
   def get_posts
-    url = "https://api.hatchways.io/assessment/blog/posts"
-    response = RestClient.get(url)
-    render json: response
+    user_search_params = params[:search]
+    user_sort_by_params = params[:sort]
+    user_directions_params = params[:directions]
+    if !user_search_params
+      render json: { error: "tags parameter required" }
+    elsif params[:sort] && (["id", "likes", "reads", "popularity"].exclude?(params[:sort]))
+      render json: { error: "Invalid sort paramater" }
+    elsif params[:directions] && (["asc", "desc"].exclude?(params[:directions]))
+      render json: { error: "Invalid directions paramater" }
+    else
+      response = RestClient.get("https://api.hatchways.io/assessment/blog/posts?tag=#{user_search_params}&sortBy=#{user_sort_by_params}&direction=#{user_directions_params}")
+      render json: response
+    end
   end
 
-  # def get_posts
-  #   url = "https://api.hatchways.io/assessment/blog/posts"
-  #   query_params = { 'tag': "tech" }
-  #   response = RestClient.get(url, query: query_params)
-  # end
+  def get_ping
+    render json: {
+      "success": true,
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get "/posts", to: "posts#get_posts"
-  resources :posts
+  scope :api do
+    get "/posts", to: "posts#get_posts"
+
+    get "/ping", to: "posts#get_ping"
+  end
 end
